b46e526548f3a2074c625803c6a3951f6bd57d18
LeonoreNdebele <127037813+LeonoreNdebele@users.noreply.github.com>
2023-10-30 00:50:54 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,25 +5,11 @@ import java.util.Scanner;
 public class Main {
 	public static void main (String[] args) {
 		
-		Employee employee = new Employee();
+		Employee employee = new Employee(1, "John Doe", 10.00f, 40);
+		employee.printInfo();
 		
 		
-        Scanner scanner = new Scanner(System.in);
-
-        employee.setEmployeeDetails();
-
-        System.out.print("Total duration: ");
-        double duration = scanner.nextDouble();
-
-        employee.setTotalDuration(duration);
-
-        double wagesPerHour = 20.0; 
-        int numberOfWeeks = 8; 
-
-        double wages = employee.wagesCalculation(wagesPerHour, numberOfWeeks);
-
-        employee.displayEmployeeDetails();
-        employee.displayWages(wages);
+       
 	}
 
 }
